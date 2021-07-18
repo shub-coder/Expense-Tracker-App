a0012e7e98780a308cd6dc9fd6e4ea754edf3383
shub-coder <mshubhendu9@gmail.com>
2021-07-18 23:23:15 +0530

    3rd complete

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,5 +1,8 @@
-import ExpenseItem from "./Components/ExpenseItem";
-function App() {
+import React from 'react';
+
+import Expenses from './Components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -21,33 +24,18 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  //Below is an imperative approach
-  /*const para = document.getElementById('para');
-  para.textContent = 'This is also visible';
-  document.getElementById('root').append(para);*/
-  return (//The way which we are adding <p> below is a declarative approach
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date={expenses[0].date}>
-      </ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title} 
-      amount={expenses[1].amount} 
-      date={expenses[1].date}>
-      </ExpenseItem>
-      <ExpenseItem
-      title={expenses[2].title} 
-      amount={expenses[2].amount} 
-      date={expenses[2].date}>
-      </ExpenseItem>
-      <ExpenseItem
-      title={expenses[3].title} 
-      amount={expenses[3].amount} 
-      date={expenses[3].date}>
-      </ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
