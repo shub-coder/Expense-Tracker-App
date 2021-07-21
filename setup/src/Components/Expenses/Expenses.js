6f8f7861c50cc8221c7ac5ba33927f85798bc3ec
shub-coder <mshubhendu9@gmail.com>
2021-07-21 10:28:52 +0530

    added filter by year

--- setup/src/Components/Expenses/Expenses.js
+++ setup/src/Components/Expenses/Expenses.js
@@ -12,6 +12,25 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+
+  let expensesContent = <p>No expense Found</p>;
+
+  if(filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+     />
+    ));
+
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -19,13 +38,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
